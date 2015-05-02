1902bb94e3215fae24a07d4d38ddded7b769efcb
Kateryna Porshnieva <k.porshnieva@gmail.com>
2015-05-02 14:46:57 +0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('AboutCtrl', function ($scope) {
+
+  });
